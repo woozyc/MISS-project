c5a30feed850c1010657854bf2990bddea979518
NewPaulWalker <linzhida19@mails.ucas.ac.cn>
2024-04-27 01:41:37 +0800

    Collect some system info.

--- app/src/main/java/com/example/collectinformation/PhoneActivity.kt
+++ app/src/main/java/com/example/collectinformation/PhoneActivity.kt
@@ -22,8 +22,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import com.example.collectinformation.util.SystemUtils
 
 class PhoneActivity : ComponentActivity() {
+    private lateinit var binding: PhoneActivity
     // onCreate方法是Activity的生命周期方法，在Activity创建时被调用
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,6 +44,15 @@ class PhoneActivity : ComponentActivity() {
         // 调用编写的DeviceInfoCollector中的函数，获取系统信息、用户信息和已安装应用程序信息
         val systemInfo = DeviceInfoCollector.getSystemInfo(this)
         val userInfo = DeviceInfoCollector.getUserInfo(this)
+
+        val basic = SystemUtils.getSysVersionInfo(this)
+        val memory = SystemUtils.getMemoInfo(this)
+        val cpu = SystemUtils.getCpuInfo()
+        val imei = SystemUtils.getIMEI(this)
+        val storage = SystemUtils.getTotalInternalMemorySize() + SystemUtils.getAvailableInternalMemorySize() +
+                SystemUtils.getTotalExternalMemorySize() + SystemUtils.getAvailableExternalMemorySize()
+
+        val mac = SystemUtils.getMacAddress(MainActivity@this)
 
         // 使用Column组件垂直排列各类信息，否则页面内字会重叠
         Column(
@@ -65,6 +76,19 @@ class PhoneActivity : ComponentActivity() {
             Text("User Information:")
             Text(userInfo)
             Spacer(modifier = Modifier.height(16.dp))
+
+            Text("基本信息:")
+            Text(basic)
+            Text("内存信息:")
+            Text(memory)
+            Text("IMEI:")
+            Text(imei)
+            Text("MAC地址:")
+            Text(mac)
+            Text("存储信息:")
+            Text(storage)
+            Text("CPU信息:")
+            Text(cpu)
         }
     }
 }
